7ac56a3c08dcd1728c3c31b011eed6abb0160264
Firmansyah Rozy <rozy.firmansyah@gmail.com>
2022-06-05 01:54:33 +0700

    add whitelist for localhost

--- server/src/utils/setHeaders.js
+++ server/src/utils/setHeaders.js
@@ -1,23 +1,27 @@
-const whitelist = ['http://localhost:3000', 'http://localhost:8080'];
+const whitelist = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "http://localhost",
+];
 
 const setHeaders = function (req, res, next) {
-  const origin = req.headers.origin;
+    const origin = req.headers.origin;
 
-  //check if the request origin is in our whitelist
-  if (whitelist.indexOf(origin) > -1) {
-    res.setHeader('Access-Control-Allow-Origin', origin);
-  }
+    //check if the request origin is in our whitelist
+    if (whitelist.indexOf(origin) > -1) {
+        res.setHeader("Access-Control-Allow-Origin", origin);
+    }
 
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With, Content-type, Authorization, Accept'
-  );
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With, Content-type, Authorization, Accept"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
 };
 
 module.exports = setHeaders;
